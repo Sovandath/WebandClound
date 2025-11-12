c855aa9954152a99e9673f0768d1d691f14290ff
Oun MengHeang <mengheang101006@gmail.com>
2025-11-12 21:17:57 +0700

    new changes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,9 +15,30 @@ from .models import (
 )
 
 class UserSerializer(serializers.ModelSerializer):
+    # Make password write-only so it won't be exposed in API responses
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'is_staff']
+        fields = ['id', 'username', 'email', 'role', 'is_staff', 'password']
+
+    def create(self, validated_data):
+        # Pop the password and create user with hashed password
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Handle password update correctly
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
